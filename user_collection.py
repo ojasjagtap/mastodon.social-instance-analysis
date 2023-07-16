0d4e8e264942f45025bec03e92dbab3dfb207a51
ojasjagtap <130205589+ojasjagtap@users.noreply.github.com>
2023-07-15 22:02:28 -0400

    Update user_collection.py

--- user_collection.py
+++ user_collection.py
@@ -1,5 +1,21 @@
-from auth import authenticate_mastodon
+from mastodon import Mastodon
 import pause
+import config
+
+
+def authenticate_mastodon():
+    client_key = config.CLIENT_KEY
+    client_secret = config.CLIENT_SECRET
+    access_token = config.ACCESS_TOKEN
+    api_base_url = "mastodon.social"
+
+    mastodon = Mastodon(
+        client_id=client_key,
+        client_secret=client_secret,
+        access_token=access_token,
+        api_base_url=api_base_url
+    )
+    return mastodon
 
 
 def save_users_to_file(filename):
